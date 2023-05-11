0b5cf4cb9932cfd650c73f01dabfabb90145b8e3
Rubick <rubick@orderly.network>
2023-05-11 12:00:26 +0800

    settlement execute

--- src/library/types/account_types.sol
+++ src/library/types/account_types.sol
@@ -7,6 +7,8 @@ import "../../../lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSe
 library AccountTypes {
     using EnumerableSet for EnumerableSet.AddressSet;
 
+    int256 constant FUNDING_MOVE_RIGHT_PRECISIONS = 100000000000000000;
+
     struct PerpPosition {
         int256 position_qty;
         int256 cost_position;
@@ -29,5 +31,19 @@ library AccountTypes {
         uint256 last_cefi_event_id;
         // perp position
         PerpPosition perp_position;
+        // reentrancy lock
+        bool has_pending_settlement_request;
+    }
+
+    // charge funding fee
+    function chargeFundingFee(PerpPosition storage position, int256 sumUnitaryFundings) public {
+        int256 accruedFeeUncoverted = position.position_qty * (sumUnitaryFundings - position.last_sum_unitary_fundings);
+        int256 accruedFee = accruedFeeUncoverted / FUNDING_MOVE_RIGHT_PRECISIONS;
+        int256 remainder = accruedFeeUncoverted - (accruedFee * FUNDING_MOVE_RIGHT_PRECISIONS);
+        if (remainder > 0) {
+            accruedFee += 1;
+        }
+        position.cost_position += accruedFee;
+        position.last_sum_unitary_fundings = sumUnitaryFundings;
     }
 }
--- src/settlement.sol
+++ src/settlement.sol
@@ -21,6 +21,8 @@ contract Settlement is Ownable, Isettlement {
     uint256 public global_event_id;
     // user_ledger
     mapping(bytes32 => AccountTypes.Account) private user_ledger;
+    // insurance_fund
+    bytes32 private insurance_fund;
 
     // require operator
     modifier onlyOperatorManager() {
@@ -31,6 +33,11 @@ contract Settlement is Ownable, Isettlement {
     // set operator_manager_address
     function set_operator_manager_address(address _operator_manager_address) public onlyOwner {
         operator_manager_address = _operator_manager_address;
+    }
+
+    // set insurance_fund
+    function set_insurance_fund(bytes32 _insurance_fund) public onlyOwner {
+        insurance_fund = _insurance_fund;
     }
 
     // Interface implementation
@@ -64,7 +71,44 @@ contract Settlement is Ownable, Isettlement {
         override
         onlyOperatorManager
     {
-        // TODO send cross-chain tx to target vault
+        // check total settle amount zero
+        int256 totalSettleAmount = 0;
+        // gas saving
+        uint256 length = settlement.settlement_executions.length;
+        PrepTypes.SettlementExecution[] calldata settlement_executions = settlement.settlement_executions;
+        for (uint256 i = 0; i < length; ++i) {
+            totalSettleAmount += settlement_executions[i].settled_amount;
+        }
+        require(totalSettleAmount == 0, "total settle amount not zero");
+
+        //
+        AccountTypes.Account storage account = user_ledger[settlement.account_id];
+        uint256 balance = account.balance;
+        account.has_pending_settlement_request = false;
+        if (settlement.insurance_transfer_amount != 0) {
+            // transfer insurance fund
+            if (int256(account.balance) + int256(settlement.insurance_transfer_amount) + settlement.settled_amount < 0)
+            {
+                // overflow
+                revert("Insurance transfer amount invalid");
+            }
+            AccountTypes.Account storage insuranceFund = user_ledger[insurance_fund];
+            insuranceFund.balance += settlement.insurance_transfer_amount;
+        }
+        // for-loop settlement execution
+        for (uint256 i = 0; i < length; ++i) {
+            PrepTypes.SettlementExecution calldata settlementExecution = settlement_executions[i];
+            AccountTypes.PerpPosition storage position = account.perp_position;
+            if (position.position_qty != 0) {
+                AccountTypes.chargeFundingFee(position, settlementExecution.sum_unitary_fundings);
+                position.cost_position += settlementExecution.settled_amount;
+                position.last_executed_price = settlementExecution.mark_price;
+            }
+            // check balance + settled_amount >= 0, where balance should cast to int256 first
+            require(int256(balance) + settlementExecution.settled_amount >= 0, "balance not enough");
+            balance = uint256(int256(balance) + settlementExecution.settled_amount);
+        }
+        account.last_cefi_event_id = event_id;
     }
 
     function execute_liquidation(PrepTypes.Liquidation calldata liquidation, uint256 event_id)
@@ -72,8 +116,23 @@ contract Settlement is Ownable, Isettlement {
         override
         onlyOperatorManager
     {
-        // TODO
-        // AccountTypes.Account storage liquidated_user = user_ledger[liquidation.account_id];
+        AccountTypes.Account storage liquidated_user = user_ledger[liquidation.account_id];
+        // for-loop liquidation execution
+        uint256 length = liquidation.liquidation_transfers.length;
+        PrepTypes.LiquidationTransfer[] calldata liquidation_transfers = liquidation.liquidation_transfers;
+        // chargeFundingFee for liquidated_user.perp_position
+        for (uint256 i = 0; i < length; ++i) {
+            AccountTypes.chargeFundingFee(liquidated_user.perp_position, liquidation_transfers[i].sum_unitary_fundings);
+        }
+        // TODO get_liquidation_info
+        // TODO transfer_liquidated_asset_to_insurance if insurance_transfer_amount != 0
+        for (uint256 i = 0; i < length; ++i) {
+            // TODO liquidator_liquidate_and_update_event_id
+            // TODO liquidated_user_liquidate
+            // TODO insurance_liquidate
+        }
+        liquidated_user.last_cefi_event_id = event_id;
+        // TODO emit event
     }
 
     function _check_cefi_down() internal pure returns (bool) {
