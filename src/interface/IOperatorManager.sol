b0b2063b1880ea704eb52be95d9236c3c289cf1d
Rubick <rubick@orderly.network>
2023-05-19 14:38:45 +0800

    seperate xchain operator

--- src/interface/IOperatorManager.sol
+++ src/interface/IOperatorManager.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8.18;
 import "../library/types/OperatorTypes.sol";
 
 interface IOperatorManager {
+    function crossChainOperatorExecuteAction(
+        OperatorTypes.CrossChainOperatorActionData actionData,
+        bytes calldata action
+    ) external;
     function operatorExecuteAction(OperatorTypes.OperatorActionData actionData, bytes calldata action) external;
 }
