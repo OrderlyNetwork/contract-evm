fd1c13c634060edc523491f51f738a0813ab9faa
lewis <sudolewis@gmail.com>
2023-06-12 18:54:19 +0800

    update crosschain manager

--- src/VaultCrossChainManager.sol
+++ src/VaultCrossChainManager.sol
@@ -6,14 +6,15 @@ import "./interface/IVaultCrossChainManager.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "crosschain/interface/IOrderlyCrossChain.sol";
 import "crosschain/utils/OrderlyCrossChainMessage.sol";
+import "./library/types/VaultTypes.sol";
+import "./library/types/EventTypes.sol";
+import "./library/Utils.sol";
 
-contract VaultCrossChainManager is
-    IOrderlyCrossChainReceiver,
-    IVaultCrossChainManager,
-    Ownable
-{
+contract VaultCrossChainManager is IOrderlyCrossChainReceiver, IVaultCrossChainManager, Ownable {
     // src chain id
     uint256 public chainId;
+    // ledger chain id
+    uint256 public ledgerChainId;
     // vault interface
     IVault public vault;
     // crosschain relay interface
@@ -37,81 +38,55 @@ contract VaultCrossChainManager is
     }
 
     // set ledgerCrossChainManager
-    function setLedgerCrossChainManager(
-        uint256 _chainId,
-        address _ledgerCrossChainManager
-    ) public onlyOwner {
+    function setLedgerCrossChainManager(uint256 _chainId, address _ledgerCrossChainManager) public onlyOwner {
         ledgerCrossChainManagers[_chainId] = _ledgerCrossChainManager;
     }
 
-    function receiveMessage(
-        bytes calldata payload,
-        uint256 srcChainId,
-        uint256 dstChainId
-    ) external override {
-        emit MessageReceived(payload, srcChainId, dstChainId);
-        // only relay can call it
+    function receiveMessage(OrderlyCrossChainMessage.MessageV1 memory message, bytes memory payload)
+        external
+        override
+    {
+        require(msg.sender == address(crossChainRelay), "VaultCrossChainManager: only crossChainRelay can call");
+        require(message.dstChainId == chainId, "VaultCrossChainManager: dstChainId not match");
         require(
-            msg.sender == address(crossChainRelay),
-            "VaultCrossChainManager: receiveMessage caller is not crossChainRelay"
+            message.payloadDataType == uint8(OrderlyCrossChainMessage.PayloadDataType.AccountTypesAccountWithdraw),
+            "VaultCrossChainManager: payloadDataType not match"
         );
 
-        OrderlyCrossChainMessage.MessageV1
-            memory message = OrderlyCrossChainMessage.decodeMessageV1(payload);
-        withdraw(message);
+        EventTypes.WithdrawData memory data = abi.decode(payload, (EventTypes.WithdrawData));
+
+        VaultTypes.VaultWithdraw memory withdrawData = VaultTypes.VaultWithdraw({
+            accountId: data.accountId,
+            sender: data.sender,
+            receiver: data.receiver,
+            brokerHash: Utils.string2HashedBytes32(data.brokerId),
+            tokenHash: Utils.string2HashedBytes32(data.tokenSymbol),
+            tokenAmount: data.tokenAmount,
+            fee: data.fee,
+            withdrawNonce: data.withdrawNonce
+        });
+
+        withdraw(withdrawData);
     }
 
     // user withdraw USDC
-    function withdraw(
-        OrderlyCrossChainMessage.MessageV1 memory message
-    ) public override onlyOwner {
-        // VaultTypes.VaultWithdraw memory data = VaultTypes.VaultWithdraw(
-        //     message.accountId,
-        //     message.userAddress,
-        //     message.tokenSymbol,
-        //     message.tokenAmount
-        // );
-        // vault.withdraw(data);
+    function withdraw(VaultTypes.VaultWithdraw memory data) internal {
+        vault.withdraw(data);
     }
 
-    function deposit(
-        VaultTypes.VaultDeposit calldata data
-    ) external override {
-        // only vault can call
-        require(
-            msg.sender == address(vault),
-            "VaultCrossChainManager: deposit caller is not vault"
-        );
-
-        // TODO broker id
-        uint256 brokerId = 123;
-
-        OrderlyCrossChainMessage.MessageV1
-            memory message = OrderlyCrossChainMessage.MessageV1({
-                version: 1,
-                method: uint8(OrderlyCrossChainMessage.CrossChainMethod.Deposit),
-                userAddress: data.userAddress,
-                srcChainId: chainId,
-                dstChainId: chainId,    // TODO subnet
-                accountId: data.accountId,
-                brokerHash: bytes32(brokerId), // TODO broker id
-                tokenHash: data.tokenHash,
-                tokenAmount: data.tokenAmount
-            });
+    function deposit(VaultTypes.VaultDeposit memory data) external override {
+        OrderlyCrossChainMessage.MessageV1 memory message = OrderlyCrossChainMessage.MessageV1({
+            method: uint8(OrderlyCrossChainMessage.CrossChainMethod.Deposit),
+            option: uint8(OrderlyCrossChainMessage.CrossChainOption.LayerZero),
+            payloadDataType: uint8(OrderlyCrossChainMessage.PayloadDataType.VaultTypesVaultDeposit),
+            srcCrossChainManager: address(this),
+            dstCrossChainManager: ledgerCrossChainManagers[ledgerChainId],
+            srcChainId: chainId,
+            dstChainId: ledgerChainId
+        });
         // encode message
-        bytes memory payload = OrderlyCrossChainMessage.encodeMessageV1(message);
+        bytes memory payload = abi.encode(data);
 
-        // send message to ledgerCrossChainManager
-        crossChainRelay.sendMessage(
-            payload,
-            chainId,
-            chainId // TODO fixme
-        );
-    }
-
-    function withdraw(
-        VaultTypes.VaultWithdraw calldata data
-    ) external override {
-        // TODO
+        crossChainRelay.sendMessage(message, payload);
     }
 }
