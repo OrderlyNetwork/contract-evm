558ebf1e746cea383940a4e6f4f202deba301302
Xiaohui Zhao <rubick@orderly.network>
2023-06-14 04:12:25 +0000

    Rubick/refactor and add test

--- test/OperatorManager.t.sol
+++ test/OperatorManager.t.sol
@@ -37,18 +37,46 @@ contract OperatorManagerTest is Test {
         operatorManager.operatorPing();
     }
 
-    // function test_deposit() public {
-    //     vm.prank(operatorAddress);
-    //     AccountTypes.AccountDeposit memory depositData = AccountTypes.AccountDeposit(
-    //         bytes32(0x847928ac5e1d1e0867035b1fcff57798ce1652ef12664ee4c387463acc502e93),
-    //         bytes32(0xfb08c0b22085b07c3787ca55e02cc585a966b0799bfef3d32fc335d7107cedef),
-    //         address(0xc764E58c95B5b702abBe775FE09dc8F653Ea9e1a),
-    //         bytes32(0x61fc29e9a6b4b52b423e75ca44734454f94ea60ddff3dc47af01a2a646fe9572),
-    //         uint256(0x1000000),
-    //         uint256(0x123),
-    //         uint256(0x1)
-    //     );
-    //     ledgerCrossChainManager.deposit(depositData);
-    //     assertEq(ledger.getUserLedgerBrokerId(depositData.accountId), depositData.brokerId);
-    // }
+    function test_EIP712() public {
+        // assume the contract of this is `0x90193C961A926261B756D1E5bb255e67ff9498A1`
+        EventTypes.WithdrawData memory data = EventTypes.WithdrawData(
+            0x1794513e2fc05828d3205892dbef3c91eb7ffd6df62e0360acadd55650c9b672,
+            0x4FDDB51ADe1fa66952de254bE7E1a84EEB153331,
+            0x4FDDB51ADe1fa66952de254bE7E1a84EEB153331,
+            'woofi_dex',
+            'USDC',
+            1000000,
+            0,
+            986532,
+            123,
+            1686648201277,
+            27,
+            0x90b44cb1c50eaca38cdbb972180d5bd6a328825aa60dc8e25f1c2cb9f1abc4a8,
+            0x5bb52b33ae0d9e1733c80da1163a33034ee433bf949e8c96beb940ece3ab5683
+        );
+        vm.chainId(986532);
+        bool succ = VerifyEIP712.verifyWithdraw(data.sender, data);
+        require(succ, "verify failed");
+    }
+
+    function testFail_EIP712() public {
+        EventTypes.WithdrawData memory data = EventTypes.WithdrawData(
+            0x1794513e2fc05828d3205892dbef3c91eb7ffd6df62e0360acadd55650c9b672,
+            0x4FDDB51ADe1fa66952de254bE7E1a84EEB153331,
+            0x4FDDB51ADe1fa66952de254bE7E1a84EEB153331,
+            'woofi_dex!!!!!!',
+            'USDC',
+            1000000,
+            0,
+            986532,
+            123,
+            1686644066387,
+            27,
+            0x4ab8ce3184c3c1ebcea11e93a4a93c3a6b01896f298cc3bcb086150f2238ca77,
+            0x540b934b704d15f567056b331fa63876ef388c0bcc6e31b3c8c98896ff6fb16d
+        );
+        vm.chainId(986532);
+        bool succ = VerifyEIP712.verifyWithdraw(data.sender, data);
+        require(succ, "verify failed");
+    }
 }
