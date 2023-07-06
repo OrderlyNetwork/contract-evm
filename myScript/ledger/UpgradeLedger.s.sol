d99ac251525756418f7461bbf8403365d6a35bcd
Xiaohui Zhao <rubick@orderly.network>
2023-07-06 07:42:39 +0000

    upgrade contract

---
+++ myScript/ledger/UpgradeLedger.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+import "openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
+import "../../src/Ledger.sol";
+
+contract UpgradeLedger is Script {
+    function run() external {
+        uint256 orderlyPrivateKey = vm.envUint("ORDERLY_PRIVATE_KEY");
+        address adminAddress = vm.envAddress("LEDGER_PROXY_ADMIN");
+        address ledgerAddress = vm.envAddress("LEDGER_ADDRESS");
+
+        ProxyAdmin admin = ProxyAdmin(adminAddress);
+        ITransparentUpgradeableProxy ledgerProxy = ITransparentUpgradeableProxy(ledgerAddress);
+
+        vm.startBroadcast(orderlyPrivateKey);
+
+        ILedger ledgerImpl = new Ledger();
+        admin.upgrade(ledgerProxy, address(ledgerImpl));
+
+        vm.stopBroadcast();
+    }
+}
