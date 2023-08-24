e762636339c5fc8bc1ef4c46f94490a4e52022e2
Lewis Liu <lewis@orderly.network>
2023-08-24 02:44:50 +0000

    Lewis/orderlyop crosschain init

--- myScript/BaseScript.s.sol
+++ myScript/BaseScript.s.sol
@@ -14,6 +14,10 @@ contract BaseScript is Script {
             return vm.envUint("MUMBAI_PRIVATE_KEY");
         } else if (network.compare("orderly")) {
             return vm.envUint("ORDERLY_PRIVATE_KEY");
+        } else if (network.compare("orderlyop")) {
+            return vm.envUint("ORDERLYOP_PRIVATE_KEY");
+        } else if (network.compare("arbitrumgoerli")) {
+            return vm.envUint("ARBITRUMGOERLI_PRIVATE_KEY");
         } else {
             revert("Invalid network");
         }
@@ -26,6 +30,10 @@ contract BaseScript is Script {
             return vm.envAddress("MUMBAI_ENDPOINT");
         } else if (network.compare("orderly")) {
             return vm.envAddress("ORDERLY_ENDPOINT");
+        } else if (network.compare("orderlyop")) {
+            return vm.envAddress("ORDERLYOP_ENDPOINT");
+        } else if (network.compare("arbitrumgoerli")) {
+            return vm.envAddress("ARBITRUMGOERLI_ENDPOINT");
         } else {
             revert("Invalid network");
         }
@@ -38,6 +46,10 @@ contract BaseScript is Script {
             return vm.envAddress("MUMBAI_RELAY_PROXY");
         } else if (network.compare("orderly")) {
             return vm.envAddress("ORDERLY_RELAY_PROXY");
+        } else if (network.compare("orderlyop")) {
+            return vm.envAddress("ORDERLYOP_RELAY_PROXY");
+        } else if (network.compare("arbitrumgoerli")) {
+            return vm.envAddress("ARBITRUMGOERLI_RELAY_PROXY");
         } else {
             revert("Invalid network");
         }
@@ -50,6 +62,10 @@ contract BaseScript is Script {
             return vm.envAddress("MUMBAI_MANAGER_PROXY");
         } else if (network.compare("orderly")) {
             return vm.envAddress("ORDERLY_MANAGER_PROXY");
+        } else if (network.compare("orderlyop")) {
+            return vm.envAddress("ORDERLYOP_MANAGER_PROXY");
+        } else if (network.compare("arbitrumgoerli")) {
+            return vm.envAddress("ARBITRUMGOERLI_MANAGER_PROXY");
         } else {
             revert("Invalid network");
         }
@@ -62,6 +78,10 @@ contract BaseScript is Script {
             return vm.envUint("MUMBAI_CHAIN_ID");
         } else if (network.compare("orderly")) {
             return vm.envUint("ORDERLY_CHAIN_ID");
+        } else if (network.compare("orderlyop")) {
+            return vm.envUint("ORDERLYOP_CHAIN_ID");
+        } else if (network.compare("arbitrumgoerli")) {
+            return vm.envUint("ARBITRUMGOERLI_CHAIN_ID");
         } else {
             revert("Invalid network");
         }
@@ -74,6 +94,10 @@ contract BaseScript is Script {
             return vm.envAddress("MUMBAI_OPERATOR_MANAGER");
         } else if (network.compare("orderly")) {
             return vm.envAddress("ORDERLY_OPERATOR_MANAGER");
+        } else if (network.compare("orderlyop")) {
+            return vm.envAddress("ORDERLYOP_OPERATOR_MANAGER");
+        } else if (network.compare("arbitrumgoerli")) {
+            return vm.envAddress("ARBITRUMGOERLI_OPERATOR_MANAGER");
         } else {
             revert("Invalid network");
         }
@@ -86,6 +110,10 @@ contract BaseScript is Script {
             return vm.envAddress("MUMBAI_VAULT");
         } else if (network.compare("orderly")) {
             return vm.envAddress("ORDERLY_VAULT");
+        } else if (network.compare("orderlyop")) {
+            return vm.envAddress("ORDERLYOP_VAULT");
+        } else if (network.compare("arbitrumgoerli")) {
+            return vm.envAddress("ARBITRUMGOERLI_VAULT");
         } else {
             revert("Invalid network");
         }
@@ -98,6 +126,10 @@ contract BaseScript is Script {
             return vm.envAddress("MUMBAI_LEDGER");
         } else if (network.compare("orderly")) {
             return vm.envAddress("ORDERLY_LEDGER");
+        } else if (network.compare("orderlyop")) {
+            return vm.envAddress("ORDERLYOP_LEDGER");
+        } else if (network.compare("arbitrumgoerli")) {
+            return vm.envAddress("ARBITRUMGOERLI_LEDGER");
         } else {
             revert("Invalid network");
         }
