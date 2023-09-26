220b40fd64a13e28b3ed475aa7355b6b070a8caf
Xiaohui Zhao <rubick@orderly.network>
2023-09-26 02:56:22 +0000

    verify when deploy

---
+++ script/ledgerV2/DeployNewLedger.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+import "openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
+import "../../src/Ledger.sol";
+import "../utils/BaseScript.s.sol";
+import "../utils/ConfigHelper.s.sol";
+
+contract DeployNewLedger is BaseScript, ConfigHelper {
+    function run() external {
+        uint256 orderlyPrivateKey = vm.envUint("ORDERLY_PRIVATE_KEY");
+        Envs memory envs = getEnvs();
+        string memory env = envs.env;
+        string memory network = envs.ledgerNetwork;
+
+        LedgerDepolyData memory config = getLedgerDeployData(env, network);
+        address ledgerAddress = config.ledger;
+        console.log("ledgerAddress: ", ledgerAddress);
+
+        vm.startBroadcast(orderlyPrivateKey);
+        ILedger ledgerImpl = new Ledger();
+        console.log("new ledgerImplAddress: ", address(ledgerImpl));
+        vm.stopBroadcast();
+        console.log("deploy done");
+    }
+}
---
+++ script/ledgerV2/DeployNewVaultManager.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+import "openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
+import "../../src/VaultManager.sol";
+import "../utils/BaseScript.s.sol";
+import "../utils/ConfigHelper.s.sol";
+
+contract DeployNewVaultManager is BaseScript, ConfigHelper {
+    function run() external {
+        uint256 orderlyPrivateKey = vm.envUint("ORDERLY_PRIVATE_KEY");
+        Envs memory envs = getEnvs();
+        string memory env = envs.env;
+        string memory network = envs.ledgerNetwork;
+
+        LedgerDepolyData memory config = getLedgerDeployData(env, network);
+        address vaultManagerAddress = config.vaultManager;
+        console.log("vaultManagerAddress: ", vaultManagerAddress);
+
+        vm.startBroadcast(orderlyPrivateKey);
+        IVaultManager vaultManagerImpl = new VaultManager();
+        console.log("new vaultManagerImplAddress: ", address(vaultManagerImpl));
+        vm.stopBroadcast();
+        console.log("deploy done");
+    }
+}
