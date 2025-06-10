5428aee40ac121321d02cbc47c51dab765628cbe
lewis <lewis@orderly.network>
2025-06-10 14:20:03 +0800

    update signature unit test

--- test/library/DelegateSwapSignature.t.sol
+++ test/library/DelegateSwapSignature.t.sol
@@ -32,12 +32,9 @@ import "forge-std/console.sol";
 import {ECDSA} from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 
 contract DelegateSwapSignatureTest is Test {
-    
-    // Sample data from user
-    address constant EXPECTED_SIGNER = 0x6a9961Ace9bF0C1B8B98ba11558A4125B1f5EA3f;
-    
-    
+
     function test_validateDelegateSwapSignature_sampleData() public {
+        address expectedSigner = 0x6a9961Ace9bF0C1B8B98ba11558A4125B1f5EA3f;
         // Sample data provided by user
         bytes memory swapCalldata = hex"83bd37f900040002031e84800702a235f1331a6e0ccccc00012a8466a3135d1E4D51B2eBe07bfb9D1f6797795b000000015D1C9cFA890Ea412EF6B909A3C43b089e8e33658000000000301020300080101010200ff0000000000000000000000000000000000000000001db0d0cb84914d09a92ba11d122bab732ac35fe0833589fcd6edb6e08f4c7c32d4f71b54bda02913000000000000000000000000000000000000000000000000";
         
@@ -69,7 +66,61 @@ contract DelegateSwapSignatureTest is Test {
 
         bytes32 digest = ECDSA.toEthSignedMessageHash(keccak256(encoded));
         address recoveredAddress = ECDSA.recover(digest, data.v, data.r, data.s);
-        assertTrue(recoveredAddress == EXPECTED_SIGNER, "Sample signature should be valid");
+        assertTrue(recoveredAddress == expectedSigner, "Sample signature should be valid");
+    }
+
+    function test_validateDelegateSwapSignature_sampleData_2() public {
+        // 0	data.tradeId	bytes32
+        // 0x0000000000000000000000000000000000000000000000000000000000000581
+        // 0	data.chainId	uint256
+        // 8453
+        // 0	data.inTokenHash	bytes32
+        // 0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa
+        // 0	data.inTokenAmount	uint256
+        // 10000000
+        // 0	data.to	address
+        // 0x19cEeAd7105607Cd444F5ad10dd51356436095a1
+        // 0	data.value	uint256
+        // 0
+        // 0	data.swapCalldata	bytes
+        // 0x83bd37f90001fde4c96c8593536e31f229ea8f37b2ada2699bb200040398968003989ae6028f5c00012a8466a3135d1e4d51b2ebe07bfb9d1f6797795b00000001e956a3a14122133daced42c598b8945eba55408300000000030102030009010101020000000700000100ff0000000000000000000000000000fde4c96c8593536e31f229ea8f37b2ada2699bb2833589fcd6edb6e08f4c7c32d4f71b54bda02913000000000000000000000000000000000000000000000000
+        // 0	data.r	bytes32
+        // 0x64039c54ef57fb713e237f7f2ec10e136b0a6476760ee1193c6bfc7c94853516
+        // 0	data.s	bytes32
+        // 0x47c7e38805ee18c92e204118e517ad3578049956d12e58fab648e8ef1be37c39
+        // 0	data.v	uint8
+        // 27
+        address expectedSigner = 0xDdDd1555A17d3Dad86748B883d2C1ce633A7cd88;
+        bytes memory swapCalldata = hex"83bd37f90001fde4c96c8593536e31f229ea8f37b2ada2699bb200040398968003989ae6028f5c00012a8466a3135d1e4d51b2ebe07bfb9d1f6797795b00000001e956a3a14122133daced42c598b8945eba55408300000000030102030009010101020000000700000100ff0000000000000000000000000000fde4c96c8593536e31f229ea8f37b2ada2699bb2833589fcd6edb6e08f4c7c32d4f71b54bda02913000000000000000000000000000000000000000000000000";
+        VaultTypes.DelegateSwap memory data = VaultTypes.DelegateSwap({
+            tradeId: 0x0000000000000000000000000000000000000000000000000000000000000581,
+            chainId: 8453,
+            inTokenHash: 0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa,
+            inTokenAmount: 10000000,
+            to: 0x19cEeAd7105607Cd444F5ad10dd51356436095a1,
+            value: 0,
+            swapCalldata: swapCalldata,
+            r: 0x64039c54ef57fb713e237f7f2ec10e136b0a6476760ee1193c6bfc7c94853516,
+            s: 0x47c7e38805ee18c92e204118e517ad3578049956d12e58fab648e8ef1be37c39,
+            v: 27
+        });
+
+        bytes memory encoded = abi.encode(
+            data.tradeId,
+            data.chainId,
+            data.inTokenHash,
+            data.inTokenAmount,
+            data.to,
+            data.value,
+            data.swapCalldata
+        );
+
+        console.logBytes(encoded);
+
+        bytes32 digest = ECDSA.toEthSignedMessageHash(keccak256(encoded));
+        address recoveredAddress = ECDSA.recover(digest, data.v, data.r, data.s);
+        console.log("recoveredAddress", recoveredAddress);
+        assertTrue(recoveredAddress == expectedSigner, "Sample signature should be valid");
     }
 
 }
